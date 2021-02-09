d9c945dd646a3df05784b858ec0b4ed66cc69d28
Jessica Yang <jessica.yang@duke.edu>
2021-02-08 21:03:07 -0500

    Applied checkstyle changes to existing code.

--- src/cellsociety/Cell.java
+++ src/cellsociety/Cell.java
@@ -9,40 +9,40 @@ package cellsociety;
  * @author Juhyoung Lee, Jessica Yang
  */
 public class Cell {
-    private final int index;
-    private int state;
 
-    /**
-     * Purpose: Constructor for Cell class.
-     * Assumptions: TODO
-     * Parameters: int index, int state.
-     * Exceptions: TODO
-     * Returns: Cell object.
-     */
-    public Cell(int index, int state) {
-        this.index = index;
-        this.state = state;
-    }
+  private final int index;
+  private int state;
 
-    /**
-     * Purpose: Returns state of the cell.
-     * Assumptions: TODO
-     * Parameters: None.
-     * Exceptions: None.
-     * Returns: int state.
-     */
-    public int getState() {
-        return state;
-    }
+  /**
+   * Purpose: Constructor for Cell class. Assumptions: TODO
+   * Parameters: int index, int state.
+   * Exceptions: TODO
+   * Returns: Cell object.
+   */
+  public Cell(int index, int state) {
+    this.index = index;
+    this.state = state;
+  }
 
-    /**
-     * Purpose: Returns index of the cell.
-     * Assumptions: TODO
-     * Parameters: None.
-     * Exceptions: None.
-     * Returns: int index.
-     */
-    public int getIndex() {
-        return index;
-    }
+  /**
+   * Purpose: Returns state of the cell.
+   * Assumptions: TODO
+   * Parameters: None.
+   * Exceptions: None.
+   * Returns: int state.
+   */
+  public int getState() {
+    return state;
+  }
+
+  /**
+   * Purpose: Returns index of the cell.
+   * Assumptions: TODO
+   * Parameters: None.
+   * Exceptions: None.
+   * Returns: int index.
+   */
+  public int getIndex() {
+    return index;
+  }
 }
