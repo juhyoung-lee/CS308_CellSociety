75a4d31dda56a3b3b737220cd81acb553a8fe886
Jessica Yang <jessica.yang@duke.edu>
2021-02-09 21:46:48 -0500

    Added PercolationCell.

--- src/cellsociety/PercolationCell.java
+++ src/cellsociety/PercolationCell.java
@@ -1,2 +1,46 @@
-package cellsociety;public class PercolationCell {
+package cellsociety;
+
+/**
+ * Purpose: Represents a cell for the Percolation simulation. Extends the Cell class.
+ * Assumptions: TODO
+ * Dependencies: TODO
+ * Example of use: TODO
+ *
+ * @author Jessica Yang
+ */
+public class PercolationCell extends Cell {
+
+  public static final int OPEN = 0;
+  public static final int PERCOLATED = 1;
+  public static final int BLOCKED = 2;
+
+  /**
+   * Purpose: Constructor for PercolationCell class.
+   * Assumptions: TODO
+   * Parameters: int state.
+   * Exceptions: TODO
+   * Returns: PercolationCell object.
+   */
+  public PercolationCell(int state) {
+    super(state);
+  }
+
+  /**
+   * Purpose: Determine new state to update to.
+   * Assumptions: TODO
+   * Parameters: int[] neighborStates.
+   * Exceptions: TODO
+   * Returns: TODO
+   * Rules taken from https://www2.cs.duke.edu/courses/compsci308/current/assign/02_simulation/PercolationCA.pdf
+   */
+  public void prepareNewState(int[] neighborStates) {
+    if (myState == OPEN) {
+      for (int state : neighborStates) {
+        if (state == PERCOLATED) {
+          nextState = PERCOLATED;
+          break;
+        }
+      }
+    }
+  }
 }
