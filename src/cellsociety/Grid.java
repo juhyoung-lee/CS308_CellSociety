9c0916c86dab0cd91541bbcf905a1ffb74b606ea
Juhyoung Lee <juhyoung.lee@duke.edu>
2021-02-13 15:40:59 -0500

    implemented methods for GameOfLife

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -1,5 +1,6 @@
 package cellsociety;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,7 +11,7 @@ import java.util.HashMap;
  * <p>Stores and updates Cells Instantiates Cell objects from XML input Updates cell statuses using
  * neighboring cell information
  *
- * <p>Assumes proper XML input configuration.XML TODO: example here!!
+ * <p>Assumes neighborCount is in XML
  *
  * <p>Depends on java.util, java.io, javax.xml, org.w3c.dom, and cell society.Cell
  *
@@ -18,23 +19,19 @@ import java.util.HashMap;
  */
 public class Grid {
 
-  private HashMap<Cell, String> neighbors;
+  private HashMap<Cell, int[]> neighbors;
   private ArrayList<Cell> grid;
-  private String gameType;
-  private int height;
-  private int width;
+  private final String gameType;
+  private final int height;
+  private final int width;
+  private final int neighborCount;
 
-
-  /**
-   * Constructor.
-   *
-   * @param gameType
-   * @param cellArrangement array of Strings representing a row of cells
-   */
   public Grid(String gameType, ArrayList<String> cellArrangement) {
     this.gameType = gameType;
     this.height = cellArrangement.size();
     this.width = cellArrangement.get(0).length();
+    // TODO: MAKE NEIGHBOR COUNT DYNAMIC
+    this.neighborCount = 8;
     setupGrid(cellArrangement);
     setupNeighbors(cellArrangement);
     printGrid();
@@ -42,6 +39,7 @@ public class Grid {
 
   /**
    * Loads updates for cells and then updates.
+   * TODO: buff up for variations
    */
   public void updateCells() {
     for (int i = 0; i < grid.size(); i++) {
@@ -53,8 +51,31 @@ public class Grid {
     }
   }
 
+  /**
+   * Returns states of cells for printing/viewing.
+   *
+   * @return array list of cell states
+   */
+  public ArrayList<Integer> viewGrid() {
+    ArrayList<Integer> cellStates = new ArrayList<>();
+    for (Cell cell : this.grid) {
+      cellStates.add(cell.getState());
+    }
+    return cellStates;
+  }
+
   private int[] pullNeighborStates(int index) {
-    return null;
+    // TODO: return type will be hashmap
+    Cell currentCell = this.grid.get(index);
+    Cell neighborCell;
+    int[] neighborIndexes = this.neighbors.get(currentCell);
+    int[] neighborStates = new int[neighborIndexes.length];
+
+    for (int i = 0; i < neighborIndexes.length; i++){
+      neighborCell = this.grid.get(neighborIndexes[i]);
+      neighborStates[i] = neighborCell.getState();
+    }
+    return neighborStates;
   }
 
   private void setupGrid(ArrayList<String> cellArrangement) {
@@ -71,31 +92,68 @@ public class Grid {
   private void setupNeighbors(ArrayList<String> cellArrangement){
     this.neighbors = new HashMap<>();
     for (int i = 0; i < grid.size(); i++) {
+      this.neighbors.put(this.grid.get(i), pullNeighborIndexes(i));
     }
   }
 
+  // used by setupNeighbors()
   private int[] pullNeighborIndexes(int index) {
-
-  }
-
-  private Cell chooseCell(int state) {
-    Cell cell;
-    switch (this.gameType) {
-      case "Conway's Game of Life":
-        cell = new GameOfLifeCell(state);
-        break;
-      default:
-        cell = null;
+    // TODO: how to make dynamic?
+    // AHH UGLY CODE
+    int[] variance = switch (this.neighborCount) {
+      case 8 -> new int[]{-1 - this.width, -1 * this.width, 1 - this.width, -1, 1, -1 + this.width, this.width, 1 + this.width};
+      case 4 -> new int[]{-1 * this.width, -1, 1, this.width};
+      default -> new int[]{};
+    };
+    ArrayList<Integer> possibleIndexes = new ArrayList<>();
+    for (int i : variance) {
+      possibleIndexes.add(variance[i] + index);
     }
-    return cell;
+    ArrayList<Integer> validIndexes = removeInvalidIndexes(index, possibleIndexes);
+    return convertIntArrayList(validIndexes);
   }
 
-  /**
-   * For testing.
-   *
-   * <p>TODO: implement Cell class TODO: ADD ROW/COLUMN COUNT CONSIDERATION TO PRINTING
-   * TODO: test find XML file on other computers/OS
-   */
+  // used by pullNeighborIndexes()
+  private int[] convertIntArrayList(ArrayList<Integer> validIndexes) {
+    int[] neighbors = new int[validIndexes.size()];
+    for (int i = 0; i < neighbors.length; i++) {
+      neighbors[i] = validIndexes.get(i);
+    }
+    return neighbors;
+  }
+
+  // used by pullNeighborIndexes()
+  private ArrayList<Integer> removeInvalidIndexes(int centerIndex, ArrayList<Integer> possibleIndexes) {
+    for (int i : possibleIndexes) {
+      if (i < 0 || i >= this.width * this.height) {
+        possibleIndexes.remove(i);
+      }
+      // checks if incrementing center index by 1 changed rows
+      // ie first/last row in index
+      if (i + 1 == centerIndex || i - 1 == centerIndex){
+       if (i / this.width != centerIndex / this.width){
+         possibleIndexes.remove(i);
+       }
+      }
+    }
+    return possibleIndexes;
+  }
+
+  // used by setupGrid()
+  private Cell chooseCell(int state) {
+    // TODO: buff up for variations
+    // variations will have to accept parameters hmMMMM
+    return switch (this.gameType) {
+      case "Conway's Game of Life" -> new GameOfLifeCell(0);
+      case "Percolation" -> new PercolationCell(0);
+      case "Fire" -> new FireCell(0, 0);
+      case "Segregation" -> new SegregationCell(0, 0);
+      case "WaTor" -> new WaTorCell(0, 0, 0, 0, 0);
+      default -> null;
+    };
+  }
+
+  // For testing.
   private void printGrid() {
     for (Cell cell : this.neighbors.keySet()) {
        System.out.print(cell.getState());
