70619af7d53b81370989a70fe68b1a0ae686aa68
Kenneth Moore III <km460@duke.edu>
2021-02-14 19:52:49 -0500

    removed hard coding from basic implementation

--- src/cellsociety/Configure.java
+++ src/cellsociety/Configure.java
@@ -4,18 +4,17 @@ import java.io.File;
 
 public class Configure {
 
-  private static final String CONFIGURATION_FILE = "data/configuration.XML";
+  private String configFile;
 
-
-  public Configure() {
-    readXML();
+  public Configure(String file) {
+    configFile = file;
   }
 
-  private void readXML() {
-    File dataFile = new File(CONFIGURATION_FILE);
+  public Game getGame() {
+    File dataFile = new File(configFile);
     while (dataFile != null) {
       try {
-        Game g = new XMLParser("media").getGame(dataFile);
+        return new XMLParser("media").getGame(dataFile);
       } catch (XMLException e) {
         // handle error of unexpected file format
         System.out.println("Error");
