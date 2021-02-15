988d64f37c16b09f54b99e89df85d7a3766b700e
Jessica Yang <jessica.yang@duke.edu>
2021-02-15 15:06:34 -0500

    Fixed WaTor bugs so it would run correctly.

--- src/cellsociety/model/wator/WaTorCell.java
+++ src/cellsociety/model/wator/WaTorCell.java
@@ -25,10 +25,8 @@ public class WaTorCell extends Cell {
 
 
   /**
-   * Purpose: Constructor for WaTorCell class.
-   * Assumptions: config will include keys "breedFish", "breedShark", "energyGain", and "energyLoss"
-   * Parameters: HashMap config.
-   * Exceptions: TODO
+   * Purpose: Constructor for WaTorCell class. Assumptions: config will include keys "breedFish",
+   * "breedShark", "energyGain", and "energyLoss" Parameters: HashMap config. Exceptions: TODO
    * Returns: WaTorCell object.
    */
   public WaTorCell(HashMap<String, Integer> config) {
@@ -37,22 +35,21 @@ public class WaTorCell extends Cell {
     sharkBreedThreshold = config.get("sharkBreedThreshold");
     energyGain = config.get("energyGain");
     energyLoss = config.get("energyLoss");
-    reset();
-  }
-
-  /** Returns settings back to default. */
-  private void reset() {
-    breedFishTime = 0;
-    breedSharkEnergy = sharkBreedThreshold /2;
+    resetMyState();
   }
 
   /**
-   * Purpose: Determine new state to update to.
-   * Assumptions: TODO
-   * Parameters: int[] neighborStates.
-   * Exceptions: TODO
-   * Returns: int type. Describes what needs to be moved, if any.
-   * Rules taken from https://beltoforion.de/en/wator/
+   * Returns settings back to default.
+   */
+  private void resetMyState() {
+    breedFishTime = 0;
+    breedSharkEnergy = sharkBreedThreshold / 2;
+  }
+
+  /**
+   * Purpose: Determine new state to update to. Assumptions: TODO Parameters: int[] neighborStates.
+   * Exceptions: TODO Returns: int type. Describes what needs to be moved, if any. Rules taken from
+   * https://beltoforion.de/en/wator/
    */
   public HashMap<String, Integer> prepareNewState(int[] neighborStates) {
     if (myState == FISH) {
@@ -68,13 +65,13 @@ public class WaTorCell extends Cell {
 
   private void fishPrepareState() {
     breedFishTime++;
+    updateMoveStateParam();
     if (breedFishTime < fishBreedThreshold) {
       setToWater();
     } else {
       nextState = FISH;
-      breedFishTime =0;
+      resetMyState();
     }
-    updateNewStateParam();
     updateStateField(FISH);
   }
 
@@ -84,66 +81,54 @@ public class WaTorCell extends Cell {
       updateStateField(NO_MOVEMENT);
     } else {
       breedSharkEnergy -= energyLoss;
+      updateMoveStateParam();
       if (breedSharkEnergy < sharkBreedThreshold) {
         setToWater();
       } else {
         nextState = SHARK;
+        resetMyState();
       }
       updateStateField(SHARK);
     }
-    updateNewStateParam();
   }
 
   /**
-   * Purpose: Accepts HashMap information with new state information.
-   * Assumptions: Grid will not pass call this method when the 'state' field is NO_MOVEMENT (-1).
-   * Parameters: HashMap object.
-   * Exceptions: TODO
-   * Returns: None.
+   * Purpose: Accepts HashMap information with new state information. Assumptions: Grid will not
+   * pass call this method when the 'state' field is NO_MOVEMENT (-1). Parameters: HashMap object.
+   * Exceptions: TODO Returns: None.
    */
   @Override
   public boolean receiveUpdate(HashMap<String, Integer> newInfo) {
     int incomingState = newInfo.get("state");
-    boolean received;
 
-    if (nextState == SHARK) {
+    if (nextState == SHARK || nextState == incomingState) { //TODO: allow fish to move into sharks?
       return false;
-    } else if (nextState == FISH) {
-      received = fishReceiveUpdate(incomingState);
-    } else {
-      received = true;
+    }
+
+    breedFishTime = newInfo.get("breedTime");
+    breedSharkEnergy = newInfo.get("breedEnergy");
+
+    if (nextState == FISH && incomingState == SHARK) {
+      breedSharkEnergy += energyGain;
     }
 
     nextState = incomingState;
-    breedFishTime = newInfo.get("breedTime");
-    breedSharkEnergy = newInfo.get("breedEnergy");
-    return received;
+    return true;
   }
 
-  private boolean fishReceiveUpdate(int newState) {
-    if (newState == SHARK) {
-      ateFish();
-      return true;
-    }
-    return false;
-  }
-
-  /** Sets cell up to be water. */
+  /**
+   * Sets cell up to be water.
+   */
   private void setToWater() {
     nextState = WATER;
-    reset();
+    resetMyState();
   }
 
-  /** Updates moveState HashMap. */
-  private void updateNewStateParam() {
+  /**
+   * Updates moveState HashMap.
+   */
+  private void updateMoveStateParam() {
     moveState.put("breedTime", breedFishTime);
     moveState.put("breedEnergy", breedSharkEnergy);
-  }
-
-  /** Adjusts breedEnergy upon SHARK cell moving into a FISH cell. */
-  private void ateFish() {
-    if (myState == SHARK) {
-      breedSharkEnergy += energyGain;
-    }
   }
 }
