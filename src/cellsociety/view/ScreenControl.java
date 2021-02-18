7d5605fd8d60824d171ebf859edd496004473aaa
KATHLEEN CHEN <kc387@duke.edu>
2021-02-18 17:00:13 -0500

    add resource and css buttons

--- src/cellsociety/view/ScreenControl.java
+++ src/cellsociety/view/ScreenControl.java
@@ -1,16 +1,17 @@
 package cellsociety.view;
 
+import cellsociety.Control;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import cellsociety.Control;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import org.w3c.dom.css.Rect;
 
 /**
  * Purpose: Creates screen display that user interacts with.
@@ -40,6 +41,7 @@ public class ScreenControl {
   private String myType;
   private ResourceBundle myResources;
   private String myStyleSheet;
+
   /**
    * Initialize the scene and add buttons and text.
    */
@@ -47,6 +49,8 @@ public class ScreenControl {
     myRoot = new Pane();
     sX = Control.X_SIZE;
     sY = Control.Y_SIZE;
+    createResourceButton();
+    createStyleButton();
     myResources = ResourceBundle.getBundle("cellsociety.view.resources.English");
     myStyleSheet = "cellsociety/view/resources/default.css";
     myScene = new Scene(myRoot, sX, sY);
@@ -54,6 +58,45 @@ public class ScreenControl {
     myBlocks = new ArrayList<>();
     createButtons();
     sim = simulationControl;
+  }
+
+  private void createStyleButton() {
+    ComboBox<String> styleButton = new ComboBox();
+    styleButton.getItems().addAll(
+            "dark",
+            "light",
+            "default"
+    );
+    styleButton.setValue("default");
+    styleButton.setLayoutX(100);
+    myRoot.getChildren().add(styleButton);
+    styleButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        myScene.getStylesheets().remove(myStyleSheet);
+        myStyleSheet = "cellsociety/view/resources/" + styleButton.getValue() + ".css";
+        myScene.getStylesheets().add(myStyleSheet);
+        resetButtons();
+      }
+    });
+  }
+
+  private void createResourceButton() {
+    ComboBox<String> resourceButton = new ComboBox();
+    resourceButton.getItems().addAll(
+            "English",
+            "Chinese",
+            "Spanish"
+    );
+    resourceButton.setValue("English");
+    myRoot.getChildren().add(resourceButton);
+    resourceButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        myResources = ResourceBundle.getBundle("cellsociety.view.resources." + resourceButton.getValue());
+        resetButtons();
+      }
+    });
   }
 
   private void createButtons() {
