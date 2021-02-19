f53a72b274828edfd13912475e6385ecf5ee1eb4
Kenneth Moore III <km460@duke.edu>
2021-02-18 20:54:30 -0500

    refctored configure into one class

--- src/cellsociety/configuration/MainTester.java
+++ src/cellsociety/configuration/MainTester.java
@@ -2,8 +2,7 @@ package cellsociety.configuration;
 
 public class MainTester {
   public static void main(String[] args){
-    Configure c = new Configure("data/XMLs/WaTor/first.XML");
-    Simulation g = c.getSimulation();
+    Simulation g = new Simulation("data/XMLs/WaTor/first.XML");
     System.out.println(g);
   }
 }
