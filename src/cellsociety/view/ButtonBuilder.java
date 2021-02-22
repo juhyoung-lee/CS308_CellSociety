3b3fd97821ea5ef5b37accef6057c2763bc3a8b6
KATHLEEN CHEN <kc387@duke.edu>
2021-02-21 22:01:47 -0500

    refactor code for buttons

--- src/cellsociety/view/ButtonBuilder.java
+++ src/cellsociety/view/ButtonBuilder.java
@@ -1,5 +1,15 @@
 package cellsociety.view;
 
+import cellsociety.Control;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+
+import java.util.ResourceBundle;
+
 /**
  * Purpose: Creates buttons for the view.
  * Assumptions: TODO
@@ -10,4 +20,64 @@ package cellsociety.view;
  */
 
 public class ButtonBuilder {
+  private Control myControl;
+  private ResourceBundle myResources;
+  private ScreenControl mySC;
+  private VBox myBox;
+  private Button slowButton;
+  private Button fastButton;
+  private Button startButton;
+  private Button pauseButton;
+  private Button stepButton;
+  private Button uploadButton;
+  private Button compareButton;
+  private Button graphButton;
+  private Button editButton;
+
+
+  public ButtonBuilder(Control control, ResourceBundle rBundle, ScreenControl sc) {
+    myControl = control;
+    mySC = sc;
+    myResources = rBundle;
+    myBox = new VBox();
+    myBox.setAlignment(Pos.CENTER_LEFT);
+    myBox.setSpacing(5.0);
+    createButtons();
+  }
+
+  public void createButtons() {
+    startButton = buttonCreation(myResources.getString("PlayButton"));
+    startButton.setOnAction(event -> myControl.start());
+    pauseButton = buttonCreation(myResources.getString("PauseButton"));
+    pauseButton.setOnAction(event -> myControl.pause());
+    stepButton = buttonCreation(myResources.getString("StepButton"));
+    stepButton.setOnAction(event -> myControl.next());
+    fastButton = buttonCreation(myResources.getString("SpeedUpButton"));
+    fastButton.setOnAction(event -> myControl.fast());
+    slowButton = buttonCreation(myResources.getString("SlowDownButton"));
+    slowButton.setOnAction(event -> myControl.slow());
+    uploadButton = buttonCreation(myResources.getString("UploadButton"));
+    uploadButton.setOnAction(event -> myControl.uploadFile());
+    graphButton = buttonCreation(myResources.getString("GraphButton"));
+    graphButton.setOnAction(event -> mySC.makeNewGraphWindow());
+    editButton = buttonCreation(myResources.getString("EditButton"));
+    editButton.setOnAction(event -> mySC.makeNewEditWindow());
+  }
+
+  private Button buttonCreation(String text) {
+    Button button = new Button(text);
+    button.getStyleClass().add("button");
+    myBox.getChildren().add(button);
+    return button;
+  }
+
+  public VBox getBox() {
+    return myBox;
+  }
+
+  public void resetButtons(ResourceBundle rBundle) {
+    myResources = rBundle;
+    myBox.getChildren().removeAll(myBox.getChildren());
+    createButtons();
+  }
 }
--- src/cellsociety/view/ScreenControl.java
+++ src/cellsociety/view/ScreenControl.java
@@ -18,7 +18,7 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -34,20 +34,11 @@ import javafx.stage.Stage;
 
 
 public class ScreenControl {
+  public static final int SCREEN_X = 700;
+  public static final int SCREEN_Y = 600;
 
-  private Pane myRoot;
+  private BorderPane myRoot;
   private Text titleText;
-  private int sX;
-  private int sY;
-  private Button slowButton;
-  private Button fastButton;
-  private Button startButton;
-  private Button pauseButton;
-  private Button stepButton;
-  private Button uploadButton;
-  private Button compareButton;
-  private Button graphButton;
-  private Button editButton;
   private Scene myScene;
   private Control sim;
   private ResourceBundle myResources;
@@ -58,24 +49,31 @@ public class ScreenControl {
   private PieChart myGraph;
   private String myType;
   private ObservableList<PieChart.Data> myData;
-
   private Map<String, Integer> paramsMap;
+  private ButtonBuilder myButtons;
+  private HBox myResourceStyle;
+  private AnchorPane myGridBox;
 
   /**
    * Initialize the scene and add buttons and text.
    */
   public ScreenControl(Control simulationControl) {
-    myRoot = new Pane();
+    sim = simulationControl;
+    myRoot = new BorderPane();
+    myResourceStyle = new HBox();
+    myResourceStyle.setSpacing(5.0);
+    myRoot.setTop(myResourceStyle);
     createResourceButton();
     createStyleButton();
     myResources = ResourceBundle.getBundle("cellsociety.view.resources.English");
     myStyleSheet = "cellsociety/view/resources/default.css";
-    myScene = new Scene(myRoot, Control.X_SIZE, Control.Y_SIZE);
+    myScene = new Scene(myRoot, SCREEN_X, SCREEN_Y);
     myScene.getStylesheets().add(myStyleSheet);
-    createButtons();
-    sim = simulationControl;
-    sX = Control.X_SIZE;
-    sY = Control.Y_SIZE;
+    myButtons = new ButtonBuilder(simulationControl, myResources, this);
+    VBox box = myButtons.getBox();
+    myRoot.setLeft(box);
+    myGridBox = new AnchorPane();
+    myRoot.setCenter(myGridBox);
   }
 
   private void createStyleButton() {
@@ -87,7 +85,7 @@ public class ScreenControl {
     );
     styleButton.setValue("default");
     styleButton.setLayoutX(100);
-    myRoot.getChildren().add(styleButton);
+    myResourceStyle.getChildren().add(styleButton);
     styleButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
@@ -107,35 +105,17 @@ public class ScreenControl {
             "Spanish"
     );
     resourceButton.setValue("English");
-    myRoot.getChildren().add(resourceButton);
+    myResourceStyle.getChildren().add(resourceButton);
     resourceButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
         sim.pause();
         myResources = ResourceBundle.getBundle("cellsociety.view.resources." + resourceButton.getValue());
-        resetButtons();
+        myButtons.resetButtons(myResources);
       }
     });
   }
 
-  private void createButtons() {
-    startButton = buttonCreation(myResources.getString("PlayButton"), sX / 12, sY / 12 + 460, myRoot);
-    startButton.setOnAction(event -> sim.start());
-    pauseButton = buttonCreation(myResources.getString("PauseButton"), sX / 2 - 32, sY / 12 + 460, myRoot);
-    pauseButton.setOnAction(event -> sim.pause());
-    stepButton = buttonCreation(myResources.getString("StepButton"), sX * 4 / 5, sY / 12 + 460, myRoot);
-    stepButton.setOnAction(event -> sim.next());
-    fastButton = buttonCreation(myResources.getString("SpeedUpButton"), sX * 3 / 5, sY / 12 + 500, myRoot);
-    fastButton.setOnAction(event -> sim.fast());
-    slowButton = buttonCreation(myResources.getString("SlowDownButton"), sX * 2 / 9, sY / 12 + 500, myRoot);
-    slowButton.setOnAction(event -> sim.slow());
-    uploadButton = buttonCreation(myResources.getString("UploadButton"), sX - 120, 0, myRoot);
-    uploadButton.setOnAction(event -> sim.uploadFile());
-    graphButton = buttonCreation(myResources.getString("GraphButton"), sX - 220, 0, myRoot);
-    graphButton.setOnAction(event -> makeNewWindow());
-    compareButton = buttonCreation(myResources.getString("CompareButton"), sX - 120, 40, myRoot);
-    compareButton.setOnAction(event -> compare());
-  }
 
   private void compare() {
     Pane comparePane = new Pane();
@@ -144,18 +124,20 @@ public class ScreenControl {
 
   }
 
-  private void makeNewWindow() {
-    graphButton = buttonCreation(myResources.getString("GraphButton"), sX - 220, 0, myRoot);
-    graphButton.setOnAction(event -> makeNewGraphWindow());
-    editButton = buttonCreation("Edit", sX - 300, 0, myRoot); //TODO: use resource bundle
-    editButton.setOnAction(event -> makeNewEditWindow());
+  private Button buttonCreation(String text, double x, double y, Pane pane) {
+    Button button = new Button(text);
+    button.setLayoutX(x);
+    button.setLayoutY(y);
+    pane.getChildren().add(button);
+    button.getStyleClass().add("button");
+    return button;
   }
 
-  private void makeNewGraphWindow() {
+  public void makeNewGraphWindow() {
     int size = Control.X_SIZE;
 
     Pane secondaryLayout = new Pane();
-    Scene secondScene = new Scene(secondaryLayout, Control.X_SIZE, Control.X_SIZE);
+    Scene secondScene = new Scene(secondaryLayout, size, size);
     String graphStyle = "cellsociety/view/resources/graph.css";
     secondScene.getStylesheets().add(graphStyle);
 
@@ -173,11 +155,10 @@ public class ScreenControl {
   }
 
   /** called after setParams() */
-  private void makeNewEditWindow() {
+  public void makeNewEditWindow() {
     Pane tertiaryLayout = new Pane();
     Scene thirdScene = new Scene(tertiaryLayout, Control.X_SIZE, Control.X_SIZE);
-    String editStyle = "cellsociety/view/resources/default.css";  //TODO: use correct css?
-    thirdScene.getStylesheets().add(editStyle);
+    thirdScene.getStylesheets().add(myStyleSheet);
 
     myWindowTitle(Control.X_SIZE, tertiaryLayout, "Edit Parameters");
 
@@ -197,8 +178,8 @@ public class ScreenControl {
 
     int i = 0;
     for (String key : paramsMap.keySet()) {
-      if (key.equals("width") || key.equals("neighborhoodSize") || key.equals("state")
-          || key.equals("height")) {
+      if (!((key.equals("width") || key.equals("neighborhoodSize") || key.equals("state")
+          || key.equals("height")))) {
         textFields.add(textFieldCreation(key, 0,
             25 + i * (Control.X_SIZE / paramsMap.size()), pane));
         i++;
@@ -237,17 +218,8 @@ public class ScreenControl {
     pane.getChildren().add(textField);
     //TODO: styleclass?
     return textField;
-
   }
 
-  private Button buttonCreation(String text, double x, double y, Pane pane) {
-    Button button = new Button(text);
-    button.setLayoutX(x);
-    button.setLayoutY(y);
-    pane.getChildren().add(button);
-    button.getStyleClass().add("button");
-    return button;
-  }
 
   public void resetGameTitleText() {
     myRoot.getChildren().remove(titleText);
@@ -276,25 +248,25 @@ public class ScreenControl {
 
 
   private void createRecGrid(String title, String type, int rows, int cols, List<Integer> cells) {
-    myRectGrid = new RectangleGrid(myStyleSheet, myScene, myRoot);
+    myRectGrid = new RectangleGrid(myStyleSheet, myScene, myGridBox);
     myRectGrid.createGrid(title, type, rows, cols, cells);
     titleText = myRectGrid.getTitleText();
   }
 
   private void createTriGrid(String title, String type, int rows, int cols, List<Integer> cells) {
-    myTriGrid = new TriangleGrid(myStyleSheet, myScene, myRoot);
+    myTriGrid = new TriangleGrid(myStyleSheet, myScene, myGridBox);
     myTriGrid.createGrid(title, type, rows, cols, cells);
     titleText = myTriGrid.getTitleText();
   }
 
   private void createHexGrid(String title, String type, int rows, int cols, List<Integer> cells) {
-    myHexGrid = new HexagonGrid(myStyleSheet, myScene, myRoot);
+    myHexGrid = new HexagonGrid(myStyleSheet, myScene, myGridBox);
     myHexGrid.createGrid(title, type, rows, cols, cells);
     titleText = myHexGrid.getTitleText();
   }
 
   private void updateGraph(List<Integer> cells) {
-    clearGraph();
+    myData.clear();
     List<Integer> sortedlist = new ArrayList<>(cells);
     Collections.sort(sortedlist);
     int max = sortedlist.get(cells.size() - 1);
@@ -305,9 +277,6 @@ public class ScreenControl {
     myGraph.setData(myData);
   }
 
-  private void clearGraph() {
-    myData.clear();
-  }
   /**
    * Updates the grid based on new cell information passed in.
    * * @param cells
@@ -335,18 +304,6 @@ public class ScreenControl {
     }
   }
 
-  private void resetButtons() {
-    myRoot.getChildren().remove(startButton);
-    myRoot.getChildren().remove(pauseButton);
-    myRoot.getChildren().remove(fastButton);
-    myRoot.getChildren().remove(slowButton);
-    myRoot.getChildren().remove(stepButton);
-    myRoot.getChildren().remove(uploadButton);
-    myRoot.getChildren().remove(graphButton);
-    myRoot.getChildren().remove(compareButton);
-    createButtons();
-  }
-
   /**
    * Returns the Scene.
    * * @return Scene
@@ -366,4 +323,3 @@ public class ScreenControl {
     paramsMap = params;
   }
 }
-
